9ed58a09e08bf130719d3d8eed93119505fda3b3
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2024-03-20 15:56:18 -0400

    Time: 36 ms (57.66%) | Memory: 16.4 MB (99.49%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -2,15 +2,8 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
 
         ''' 
-        recursively divide n /2 until it reaches 1 or it becomes not
-        divisible
+        using bitwise
         '''
 
-        if n == 1:
-            return True
-
-        if n % 2 != 0 or n == 0:
-            return False
-
-        return self.isPowerOfTwo(n/2) 
+        return n > 0 and (n & (n -1)) == 0
         
