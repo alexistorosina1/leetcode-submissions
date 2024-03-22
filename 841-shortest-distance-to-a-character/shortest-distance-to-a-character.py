d4061035f33d6624fa431444343d524b302b4fc1
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2024-03-21 21:50:30 -0400

    Time: 92 ms (5.04%) | Memory: 16.8 MB (16.29%) - LeetSync

--- 841-shortest-distance-to-a-character/shortest-distance-to-a-character.py
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.py
@@ -4,22 +4,24 @@ class Solution:
         [11, 11, 11, 0, 11, 0, 0, 11, 11, 11, 11, 0]
         '''
 
-        answer = []
-        indx_of_c = []
-        for i in range(len(s)):
-            if s[i] != c:
-                answer.append(len(s))
-            else:
-                indx_of_c.append(i)
-                answer.append(0)
-        for i in range(len(answer)): # the indexs 0 1 2 3 4 5 ... 11
-            shortest_dist = len(s)
-            for n in indx_of_c:
-                dist = abs(n-i)
-                if(shortest_dist > dist):
-                    shortest_dist = dist
-            answer[i] = shortest_dist # updating the values here directly
-        return answer
+        def helper(i, distance):
+        # Set the basecase: Out of bound or previous distance away from c was smaller.
+            if i < 0 or i > len(s) - 1 or res[i] <= distance:
+                return 
+
+        # Set the distance for index and recursively call left and right of index.
+            res[i] = distance
+            helper(i + 1, distance + 1)
+            helper(i - 1, distance + 1)
+        
+      # Call the recursive function at each point where we find c in string
+        res = [len(s) for i in range(len(s))]
+        for i, char in enumerate(s):
+            if char == c:
+                helper(i, 0)
+        
+        # Return the result
+        return res
 
 
 
