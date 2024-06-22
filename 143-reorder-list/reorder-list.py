0ff38b5d2eecfb7c264a01b45ba827741d78f08e
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2024-06-21 22:38:16 -0400

    Time: 51 ms (80.61%) | Memory: 24.6 MB (73.25%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -5,13 +5,13 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        # find the middle of the list using fast, slow pointers
+        # find the middle of the list
         slow, fast = head, head
         while fast and fast.next:
-            slow = slow.next 
+            slow = slow.next
             fast = fast.next.next
 
-        # reverse the second half of the list
+        # reverse the second half
         second = slow.next
         prev = slow.next = None
         while second:
@@ -19,12 +19,15 @@ class Solution:
             second.next = prev
             prev = second
             second = temp
-        
+
         # merge both lists
-        first, second = head, prev
-        while second:
-            tmp1, tmp2 = first.next, second.next
-            first.next = second
-            second.next = tmp1
-            first, second = tmp1, tmp2
+        left, right = head, prev
+        while right:
+            tmp1, tmp2 = left.next, right.next
+            left.next = right
+            right.next = tmp1
+            left,right = tmp1, tmp2
+
+
+
         
