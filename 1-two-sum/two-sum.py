65c05b2789aa00ec7ad216670c444781b54494e1
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2024-07-16 13:20:55 -0400

    Time: 49 ms (95.61%) | Memory: 17.8 MB (25.61%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,23 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # create a hash to store elements with their indices
-        # check for complements when iterating
-        # check if target - nums[i] exists in the hash
-        # if it exists, return the indices of nums[i] and the complement from the hash
-        # else add the current element to the hash
-
-        hash = {}
+        hashmap = {}
 
         for i, num in enumerate(nums):
             complement = target - num
 
-            if complement in hash:
-                return [hash[complement], i]
-            else:
-                hash[num] = i
-
-        return None
-
-
-
-        
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[num] = i
