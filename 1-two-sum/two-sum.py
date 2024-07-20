cb89bf97e8ab49ecd226c6ff7b9faaa0a85d4083
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2024-07-19 22:44:03 -0400

    Time: 52 ms (89.92%) | Memory: 17.7 MB (25.04%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # create a hashmap
+        # iterate through nums
+        # check if the complement is in nums
+        # return if so, if not add to hash
+
         hashmap = {}
 
         for i, num in enumerate(nums):
             complement = target - num
+
             if complement in hashmap:
                 return [hashmap[complement], i]
+
             hashmap[num] = i
