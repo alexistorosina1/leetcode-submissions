489edaee1dd0b016b605e02281cfa9248bdedcde
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2024-07-24 17:15:01 -0400

    Time: 54 ms (31.81%) | Memory: 17 MB (41.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,17 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
+        # use a hashmap to keep track of each char in s
+        # use a hashmap to keep track of each char in t
+        # compare both
         hashmap_s = {}
         hashmap_t = {}
-
+        
         for char in s:
             hashmap_s[char] = hashmap_s.get(char, 0) + 1
         
         for char in t:
             hashmap_t[char] = hashmap_t.get(char, 0) + 1
-        
-        return hashmap_s == hashmap_t
 
-        
+        return hashmap_s == hashmap_t
