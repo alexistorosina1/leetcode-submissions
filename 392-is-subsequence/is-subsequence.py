925e0d2615792c3c4b53b9e80503de46de17f4f4
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2024-07-25 12:55:50 -0400

    Time: 42 ms (17.41%) | Memory: 16.7 MB (19.85%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,10 +1,16 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i, j = 0,0
+        # use two pointers
+        # if both pointers == the same 
+        # move s pointer 
+        # t pointer moves regardless
+
+
+        i, j = 0, 0
 
         while i < len(s) and j < len(t):
             if s[i] == t[j]:
                 i += 1
             j += 1
-        
+
         return i == len(s)
