ba0700d723683117a76102e15b983410aefdf6af
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2024-07-25 00:25:23 -0400

    Time: 502 ms (70.08%) | Memory: 18.3 MB (24.63%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -1,10 +1,16 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
+        # initiate the max_right as -1
+        # iterate backwards from the end of the list
+        # get the new_max from arr[i] and max_right
+        # change arr[i] to be max_right
+        # calculate the new max btwn max_right and new_max
+        
         max_right = -1
 
         for i in range(len(arr) -1, -1, -1):
-            current = arr[i]
+            new_max = arr[i]
             arr[i] = max_right
-            max_right = max(current, max_right)
+            max_right = max(max_right, new_max)
 
         return arr
