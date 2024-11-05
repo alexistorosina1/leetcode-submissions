3a255ae82451c859d5a76fc638f167d7ac35ae3b
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2024-11-05 10:42:39 -0500

    Time: 0 ms (100.00%) | Memory: 16.7 MB (84.45%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -1,8 +1,13 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
+        # arr = [1, 2, 1]
+        # iterate twice and populate arr
+        # ans = [1, 2, 1]
+        # but the output has to be [1, 2, 1, 1, 2, 1]
         ans = []
 
-        for i in range(2):
+        for _ in range(2):
             for num in nums:
                 ans.append(num)
+        
         return ans
