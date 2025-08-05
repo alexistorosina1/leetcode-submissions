fbe3466a8a824fd149c67779bb9ce13ed084af06
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2025-08-05 14:29:08 -0400

    Time: 0 ms (100.00%) | Memory: 19 MB (33.06%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,17 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # create a dictionary that contains num and its index
-        # as we iterate through the array we look for the target nums complement
-        # if complement is not in the dictionary then place it into the dictionary
-        # else return the complement and i
-
-        nums_dict = {}
+        hashmap = {}
 
         for i, num in enumerate(nums):
-            complement = target - num
-
-            if complement in nums_dict:
-                return [nums_dict[complement], i]
-
-            nums_dict[num] = i
-        
+            diff = target - num
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            hashmap[num] = i
