88755f04d8080969c6f8407f0ac3f4341dd83f43
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2025-10-29 13:51:45 -0400

    Time: 11 ms (72.04%) | Memory: 17.7 MB (86.40%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,21 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # iterate through s and t
-        # create 2 dictionaries containing each char and char count
-        # compare dictionaries is dict_s == dict_t return true
-    
-        dict_s = {}
-        dict_t = {}
 
         if len(s) != len(t):
             return False
+            
+        hash_s = {}
+        hash_t = {}
 
         for char in s:
-            dict_s[char] = dict_s.get(char, 0) + 1
-
-        for char in t:
-            dict_t[char] = dict_t.get(char, 0) + 1
+            hash_s[char] = hash_s.get(char, 0) + 1
         
-        return dict_t == dict_s
+        for char in t:
+            hash_t[char] = hash_t.get(char, 0) + 1
 
-        print(dict_s)
+        return hash_s == hash_t
