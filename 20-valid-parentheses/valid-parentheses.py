80726705ed8706262f0a8b82171b325bc3a53f1d
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2026-02-24 22:14:52 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (78.55%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,21 +1,23 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # ["()"]
+        # stack to keep track of open parenthesis
+        # hashmap of corresponding parenthesis
         stack = []
-        closing_bracket = {
+        hash = {
             ")": "(",
             "]": "[",
             "}": "{"
         }
 
         for char in s:
-            if char in closing_bracket:
-                if stack and stack[-1] == closing_bracket[char]:
+            if char in hash:
+                if stack and stack[-1] == hash[char]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(char)
+        
         return True if not stack else False
-            
-            
+
+                
