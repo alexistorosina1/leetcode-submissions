a612f33d1637114ea664004050705afb79497661
Alexis Torosina <109489801+alexistorosina1@users.noreply.github.com>
2026-03-11 11:06:02 -0400

    Time: 0 ms (100.00%) | Memory: 20.9 MB (92.98%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.py
@@ -8,8 +8,9 @@ class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if not root or root.val == val:
             return root
-        
+
         if root.val < val:
             return self.searchBST(root.right, val)
         else:
             return self.searchBST(root.left, val)
+        
